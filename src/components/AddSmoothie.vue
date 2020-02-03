8cfbfd88b3492f1125645a4724977723df746282
nakalua <luanadez13@gmail.com>
2020-02-02 23:21:05 -0300

    add smoothie no firebase

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -6,11 +6,16 @@
       <label for="title">Smoothie title:</label>
       <input type="text" name="title" v-model="title">
     </div>
+    <div v-for="(ing, index) in ingredients" :key="index">
+      <label for="ingredients">Ingredients:</label>
+      <input type="text" name="ingredients" v-model="ingredients[index]">
+    </div>
     <div class="field add-ingredient">
       <label for="add-ingredient">Add an ingredient</label>
-      <input type="text" name="add-ingredient" @keydown.tab="addIng" v-model="another">
+      <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
     </div>
     <div class="field center-align">
+      <p v-if="feedback" class="red-text">{{ feedback }}</p>
       <button class="btn pink">Add Smoothie</button>
     </div>
   </form>
@@ -18,23 +23,49 @@
 </template>
 
 <script>
+  import db from '@/firebase/init'
+  import slugify from 'slugify'
+
   export default {
     name: 'AddSmoothie',
     data() {
       return {
         title: null,
         another: null,
-        ingredients: []
+        ingredients: [],
+        feedback: null,
+        slug: null
       }
     },
     methods: {
       addSmoothie() {
-        console.log(this.title)
+        if(this.title) {
+          this.feedback = null
+          // create a slug
+          this.slug = slugify(this.title, {
+            replacement: '-',
+            remove: /[$*_+~.()'"!-:@]/g,
+            lower: true
+          })
+          db.collection('smoothies').add({
+            title: this.title,
+            ingredients: this.ingredients,
+            slug: this.slug
+          })
+          .then(() => {
+            this.$router.push({ name: 'Index' })
+          })
+        } else {
+          this.feedback = 'You must enter a smoothie title'
+        }
       },
       addIng() {
         if(this.another) {
           this.ingredients.push(this.another)
-          console.log(this.ingredients)
+          this.another = null
+          this.feedback = null
+        } else {
+          this.feedback = 'You must enter a value to add an ingredient'
         }
       }
     }
